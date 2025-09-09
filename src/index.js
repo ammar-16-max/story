e28ad6d4675b241075df5b904ec18314d0c04b08
ammar <hamdm2256@gmail.com>
2025-09-09 17:54:32 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import '@fortawesome/fontawesome-free/js/all.min';
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+});
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require('path');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 9000,
+    hot: false, 
+    devMiddleware: {
+      writeToDisk: true, 
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          minimize: !isDevelopment, 
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /bootstrap\.min\.css$/i,
+        use: [
+          isDevelopment ? 'style-loader' : {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          "css-loader"],   
+      },
+      {
+        test: /bootstrap\.min\.css$/i,
+                use: [
+          isDevelopment ? 'style-loader' : {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          "rtlcss-loader"], 
+      },
+         {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+         filename: './images/[name][ext]'
+       }
+      },
+         {
+        test: /\.(svg|eot|woff|woff2|ttf)$/i,
+        type: 'asset/resource',
+        generator: {
+         filename: './fonts /[name][ext]'
+       }
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    !isDevelopment && new MiniCssExtractPlugin({
+      filename: 'css/style.css'
+    }),
+  ].filter(Boolean),
+  
+  optimization: {
+    minimizer: [
+      ...(!isDevelopment ? [new CssMinimizerPlugin()] : [])
+    ],
+  }
+};       
