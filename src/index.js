1a6dedc1d1068a813f8ded8ac6e055d7768d4fc9
ammar <hamdm2256@gmail.com>
2025-10-01 15:42:19 +0300

    brand products done

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@ import '@fortawesome/fontawesome-free/js/all.min';
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
+  });
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener("click", () => {
+    alert('!اضيف هذا المنتج الي عربه الشراء');
+    })
 });
