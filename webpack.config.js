8b98c1f6fcc2a75d746983786453e850c7520559
ammar <hamdm2256@gmail.com>
2025-11-16 09:09:30 +0300

    payment page done

--- webpack.config.js
+++ webpack.config.js
@@ -81,9 +81,13 @@ module.exports = {
       filename:"product.html",
       template: './src/product.html'
     }),
-        new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       filename:"checkout.html",
       template: './src/checkout.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename:"payment.html",
+      template: './src/payment.html'
     }),
     !isDevelopment && new MiniCssExtractPlugin({
       filename: 'css/style.css'
